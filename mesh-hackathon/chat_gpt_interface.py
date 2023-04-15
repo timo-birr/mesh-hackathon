ee70bdd88911d1da51bc99c2ea8944e83f325809
timo_birr <ugtrs@student.kit.edu>
2023-04-15 16:33:38 +0200

    Improved accuracy

--- mesh-hackathon/chat_gpt_interface.py
+++ mesh-hackathon/chat_gpt_interface.py
@@ -7,11 +7,13 @@ import deepl
 data_dir = "data_processed"
 index_filename = "index.json"
 key_filename = "key"
+id_index = "index.json"
 
-max_input_size = 4096
-num_outputs = 512
-max_chunk_overlap = 20
-chunk_size_limit = 600
+
+max_input_size = 65536
+num_outputs = 4096
+max_chunk_overlap = 30
+chunk_size_limit = 50
 
 with open(key_filename, 'r') as file:
     key_string = file.read()
@@ -47,7 +49,33 @@ class ChatBot:
 
     def __init__(self, index, llm):
         self.index = index
+        # self.id_bot = id_bot
         self.llm = llm
+        self.context = []
+
+    def prepare_input(self, input_text):
+        result = "Beantworte die Frage mit einer Liste von Produkten. " + input_text + ". Was is deren ID?"
+        print(result)
+        return result
+
+    def prepare_output(self, output_text):
+        output_string = output_text
+        output_string = output_string.replace("ID:", "")
+        last_index = 0
+        while "(" in output_string[last_index:]:
+            start_index = output_string.find("(", last_index)
+            end_index = output_string.find(")", last_index)
+            last_index = end_index + 2
+            if end_index > start_index:
+                value = output_string[start_index + 1:end_index]
+                previous_len = len(value)
+                value = value.replace(" ", "")
+                url = f"<a href='https://www.fischer.de/de-de/produkte/{value}' target='_blank'> Link</a>"
+                last_index += (len(url) - previous_len)
+                output_string = output_string[:start_index] + "(" + url + output_string[end_index:]
+        output_string = output_string.replace("\n", "<br>")
+        print(output_string)
+        return output_string
 
 
     def chat(self, input_text):
@@ -55,8 +83,13 @@ class ChatBot:
         #translator = deepl.Translator(auth_key)
         #result = translator.translate_text(text, target_lang=target_language)
         #translated_text = result.text
-        response = self.index.query(input_text, response_mode="compact")
-        return response.response
+        self.context.append("Nutzer: " + input_text)
+        input_text = self.prepare_input(input_text)
+        response = self.index.query(input_text,similarity_top_k=20,response_mode="compact")
+        response = self.prepare_output(response.response)
+        # response = self.id_bot.query(input_text, response_mode="compact")
+        self.context.append("Assistent: " + response)
+        return response
 
 
 def main():
@@ -64,21 +97,34 @@ def main():
     llm = LLMPredictor(ChatOpenAI(temperature=0.0, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
     index_path = os.path.join(data_dir, index_filename)
 
+
     if not os.path.isfile(index_path):
         print("Indexing")
         construct_index(data_dir, llm)
         print("done")
 
+    """"if not os.path.isfile(os.path.join("mappings", id_index)):
+        print("Indexing")
+        construct_index("mappings", llm)
+        print("done")"""""
+
     index = GPTSimpleVectorIndex.load_from_disk(index_path)
+    #id_bot = GPTSimpleVectorIndex.load_from_disk(os.path.join("mappings", id_index))
     chatbot = ChatBot(index, llm)
 
     iface = gr.Interface(fn=chatbot.chat,
                          inputs=gr.components.Textbox(lines=7, label="Enter your text"),
-                         outputs="text",
+                         outputs=gr.outputs.HTML(),
                          title="Custom-trained AI Chatbot")
 
     iface.launch(share=False)
 
+def test():
+    test = "-Test (ID: 205) \n - BLA (ID: 42) \n -xkd (ID: 50)"
+    bot = ChatBot(None, None)
+    print(bot.prepare_output(test))
+
 
 if __name__ == "__main__":
     main()
+    #test()
